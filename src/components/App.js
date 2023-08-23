2f9baf1a2968da6fbb7a92e8c49e44584863d5a1
Manoj Saini <manojsaini513@gmail.com>
2023-08-23 09:27:05 +0000

    done 7

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,7 @@ import Tabs from "./Tabs";
 const tabs = [
   {title:"Tab 1" , content:"Tab 1"},
   {title:"Tab 2" , content:"Tab 2"},
-  {title:"Tab 3" , content:"Tab 3"}
+  {title:"Tab 3" , content:"Tab 3"},
 ]
 
 const App = () => {
