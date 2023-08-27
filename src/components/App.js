19c91f785f200774f11bb9fa0c7a8737ef2f14d4
Manoj Saini <manojsaini513@gmail.com>
2023-08-27 09:40:51 +0000

    final 1

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import './../styles/App.css';
 import Tabs from "./Tabs";
 const App = () => {
-  
+
   const arr = [
     {
         title:"Tab 1", content:"This is the content for Tab 1.",
@@ -14,7 +14,8 @@ const App = () => {
         title:"Tab 3", content:"This is the content for Tab 3.",
     },
 
-];
+]
+
   return (
     <div>
         {/* Do not remove the main div */}
